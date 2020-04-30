39cc595826368d427aff7ff306c66b9d59374093
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-30 16:58:53 -0500

    data from getFoods is there in json

--- src/FoodContainer/index.js
+++ src/FoodContainer/index.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react'
 
 export default class FoodContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      foods: []
+    }
+  }
+
+  componentDidMount() {
+    this.getFoods()
+  }
+
+  getFoods = async () => {
+    try {
+      const url = process.env.REACT_APP_API_URL + "/api/v1/foods/"
+      console.log("will fetch data from (url):", url);
+      const foodResponse = await fetch(url)
+      console.log("here's the fetch call:", foodResponse);
+      const foodsJson = await foodResponse.json()
+      console.log("heres data from getFoods in json", foodsJson);
+    } catch(err) {
+      console.error("error retrieving FOOD DATA", err);
+    }
+  }
+
 	render() {
 		return(
 			<h2>Food Container</h2>
