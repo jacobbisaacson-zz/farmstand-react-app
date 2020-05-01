030f88990ff9ac52986d31943887fa86b350a356
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-05-01 10:16:14 -0500

    register / login for up and good, have link to sign up, going to add all farmers link and put in the nav conditiaonlly along with the log in form so if youre not already registered, you see the login form and the nav bar with the one link in it

--- src/LoginRegisterForm/index.js
+++ src/LoginRegisterForm/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Form, Button, Label } from 'semantic-ui-react'
+import '../index.css'
 
 export default class LoginRegisterForm extends Component {
 	constructor() {
@@ -7,14 +8,42 @@ export default class LoginRegisterForm extends Component {
 
 		this.state = {
 			username: '',
-			password: ''
+			password: '',
+      action: 'Login'
 		}
 	}
+
+  switchForm = () => {
+    if(this.state.action === "Login") {
+      this.setState({ action: "Register" })
+    } else {
+      this.setState({ action: "Login"})
+    }
+  }
+
+
+// below -- MAKE IT SO THAT THE ONLY THINGS YOU CAN SEE IF YOU'RE NOT LOGGED IN IS THE 
+// REGISTRATION FORM & A LINK SAYING, ALL FARMERS (ALONG WITH THE STUFF ALREADY BELOW!)
+
 
   render() {
     return (
       <React.Fragment>
+        <h2>{this.state.action} here</h2>
         <Form>
+          {
+            this.state.action === "Register"
+            &&
+            <React.Fragment>
+              <Label>Username:</Label>
+              <Form.Input
+                type="text"
+                name="username"
+                placeholder="Enter Username: "
+                value={this.state.username}
+              />
+            </React.Fragment>
+          }
           <Label>Username:</Label>
           <Form.Input
             type="text"
@@ -29,9 +58,27 @@ export default class LoginRegisterForm extends Component {
             placeholder="Enter Password: "
             value={this.state.password}
           />
-          <Button type="Submit">Log In</Button>
+          <Button type="Submit">
+            { this.state.action === "Login" ? "Log in" : "Sign up!" }
+          </Button>
         </Form>
+        {
+          this.state.action === "Login"
+          ?
+          <p>
+            Want to Join? Sign up <span className="fake-link" onClick={this.switchForm}>here</span>.
+          </p>
+          :
+          <p>
+            Already joined? Log in <span className="fake-link" onClick={this.switchForm}>here</span>.
+          </p>
+        }
       </React.Fragment>
     )
   }
 }
+
+
+
+
+
